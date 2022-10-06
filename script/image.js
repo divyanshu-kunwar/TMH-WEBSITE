e41c8b792d4653ce2e3fdb295002c728a6badb7b
Himanshu Sharma <103306399+IVYLIFE@users.noreply.github.com>
2022-10-06 05:47:49 +0530

    Changes Image links of Gallery Page

--- script/image.js
+++ script/image.js
@@ -1,77 +1,77 @@
 const Imagedata = {
     1:{
         caption:"Night View",
-        src:"../media/18.jpeg",
+        src:"./media/18.jpeg",
         aspectRatio: '5/3'
     },
     2:{
         caption : "Main Entrance Of TMH",
-        src:"../media/1.jpg",
+        src:"./media/1.jpg",
         aspectRatio: '5/3'
     },
     3:{
         caption : "Spring Season",
-        src:"../media/2.jpg",
+        src:"./media/2.jpg",
         aspectRatio: '5/3'
     },
     4:{
         caption : "Night View As Seen From Hostel Room",
-        src:"../media/3.jpeg",
+        src:"./media/3.jpeg",
         aspectRatio: '5/3'
     },
     5:{
         caption : "TMH",
-        src:"../media/14.jpeg",
+        src:"./media/14.jpeg",
         aspectRatio: '5/3'
     },
     6:{
         caption:"Hostel Building As Viewed From HSS Dept. Road",
-        src:"../media/16.jpeg",
+        src:"./media/16.jpeg",
         aspectRatio:'2/3'
     },
     7:{
         caption:"Outside Scenic view as seen from Hostel Room",
-        src:"../media/10.jpeg",
+        src:"./media/10.jpeg",
         aspectRatio:'2/3'
     },
     8:{
         caption:"TMH Hostel Building Sorrounded By Flora",
-        src:"../media/12.jpeg",
+        src:"./media/12.jpeg",
         aspectRatio:'2/3'
     },
     9:{
         caption:"TMH Hostel",
-        src:"../media/15.jpeg",
+        src:"./media/15.jpeg",
         aspectRatio:'2/3'
     },
     10:{
         caption:"Janamashtami Celebration",
-        src:"../media/5.jpeg",
+        src:"./media/5.jpeg",
         aspectRatio:'2/3'
     },
     11:{
         caption:"Janamashtami Celebration",
-        src:"../media/9.jpeg",
+        src:"./media/9.jpeg",
         aspectRatio:'5/3'
     },
     12:{
         caption:"Hostel March Platoon 2020",
-        src:"../media/19.jpg",
+        src:"./media/19.jpg",
         aspectRatio:'5/3'
     },
     13:{
         caption:"TMH Kabaddi Team 2020",
-        src:"../media/20.jpg",
+        src:"./media/20.jpg",
         aspectRatio:'5/3'
     },
     14:{
         caption:"Republic Day Cycle Rally by TMH 2020",
-        src:"../media/21.jpeg",
+        src:"./media/21.jpeg",
         aspectRatio:'5/3'
     },
     15:{
         caption:"Bihu Team TMH 2019",
-        src:"../media/22.jpg",
+        src:"./media/22.jpg",
         aspectRatio:'5/3'
     },
 
